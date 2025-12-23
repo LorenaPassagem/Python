d6dcec5bcd8423556fb788b18d6d4eba07e38c1b
Lorena <lorenapassagem@gmail.com>
2025-12-23 10:23:09 -0300

    Estudos de Python/ Pandas
    Essa aula criei uma series que faz a média com a idades que esta em uma lista.

---
+++ 02_series.py
@@ -0,0 +1,12 @@
+#%%
+import pandas as pd
+idades = [18, 22, 25, 30, 35, 40]
+
+series_idades = pd.Series(idades)
+series_idades
+
+# %%
+## metodos das series
+
+print(" Media ", series_idades.mean())
+# %%
